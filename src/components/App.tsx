f716529230e0dd43bc0a76f2305cbac73be70e2a
Stu Last <stuart@spycedconcepts.uk>
2021-12-12 04:53:24 +0000

    Implement react scaffold

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import '../main.css';
+
+const App: React.FC = () => {
+  return (
+    <Fragment>
+      <div className='container'>
+        <h2>Greetings from Earth</h2>
+      </div>
+      ;
+      <hr />
+    </Fragment>
+  );
+};
+
+export default App;
